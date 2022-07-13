c7901deef4edffe0361339bfce6af229b120d66c
Adrien <adrien.biz@gmail.com>
2022-07-13 22:22:49 +0400

    Add basic mathematical functions

---
+++ scripts/scripts.js
@@ -0,0 +1,27 @@
+function operate(operator, x, y) {
+    if (operator === '+') {
+        return add(x, y);
+    } else if (operator === '-') {
+        return subtract(x, y);
+    } else if (operator === 'x') {
+        return multiply(x, y);
+    } else if (operator === '/') {
+        return divide(x, y);
+    }
+}
+
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
