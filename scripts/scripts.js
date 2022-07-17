b93e512ed3b3808afde1724f9f28008279fb320a
Adrien <adrien.biz@gmail.com>
2022-07-17 14:48:08 +0400

    Store numbers and operators in object

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,49 @@
+// const displayBtns = document.querySelectorAll('.display');
 const digits = document.querySelectorAll('.digits');
+const operators = document.querySelectorAll('.operators');
 const displayTop = document.querySelector('.display-top');
+const ac = document.querySelector('#AC');
+const equal = document.querySelector('#equal');
 
-digits.forEach(digit => digit.addEventListener('click', displayDigits));
+let input = {};
+let numberCount = 0;
+let operatorCount = 0;
 
-function displayDigits(e) {
-    console.log(e.target.textContent);
-    displayTop.textContent += e.target.textContent;
+digits.forEach(digit => digit.addEventListener('click', displayInput));
+operators.forEach(operator => operator.addEventListener('click', displayOperator));
+// displayBtns.forEach(displayBtn => displayBtn.addEventListener('click', displayInput));
+ac.addEventListener('click', clear);
+equal.addEventListener('click', displayResult);
+
+function displayInput(e) {
+        value = `value${numberCount}`;
+        if (!input[value]) {
+            input[value] = '';
+        }
+        input[value] += e.target.textContent;
+        displayTop.textContent = input[value];
+        console.log(input);
+}
+
+function displayOperator(e){
+    operator = `operator${operatorCount}`;
+    if (!input[operator]) {
+        input[operator] = '';
+    }
+    input[operator] += e.target.textContent;
+    displayTop.textContent += input[operator];
+    console.log(input);
+    numberCount++;
+    operatorCount++;
+}
+
+function clear() {
+    input = '';
+    displayTop.textContent = '';
+}
+
+function displayResult() {
+    console.log(parseInt(displayTop.textContent));
 }
 
 function operate(x, operator, y) {
